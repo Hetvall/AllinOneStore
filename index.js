e9a9f1048aff0da13295b2f3b84a814ebab12874
James Orozco <jamesorozcoh@gmail.com>
2023-12-07 17:00:09 -0500

    update

--- index.js
+++ index.js
@@ -1,68 +1,80 @@
-const api = "https://fakestoreapi.com/products";
+const URL = "https://fakestoreapi.com/products";
 const products = document.querySelector(".products");
 const filterInput = document.querySelector(".filter");
+let page = 1;
+let limit = 5;
 let productList = [];
 
 // Promise
-const getProducts = async () => {
-  try {
-    const response = await fetch(api);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
-
-// Reload event
-document.addEventListener("DOMContentLoaded", async () => {
-  productList = await getProducts();
-  displayProducts(productList);
-
-// Category Filter
-  filterInput.addEventListener("keyup", () => {
-    const filterValue = filterInput.value.trim().toLowerCase();
-    const filteredProducts = productList.filter((product) =>
-      product.category.toLowerCase().includes(filterValue)
-    );
-    if (filterValue) {
-      displayProducts(filteredProducts);
-    } else {
-      alert("Please enter a valid category");
-    }
-  });
-});
+async function getProducts(page, limit) {
+  const response = await fetch(`${URL}?limit=${limit}&page=${page}`);
+  const data = await response.json();
+  return data;
+}
 
 // List of products
 function displayProducts(productsToDisplay) {
-  products.innerHTML = "";
-
   productsToDisplay.forEach((product) => {
     let productItem = document.createElement("div");
     productItem.classList.add("product-item");
 
     const id = document.createElement("text");
     id.innerHTML = `Product ID: ${product.id}`;
-    products.appendChild(id);
+    productItem.appendChild(id);
 
     const title = document.createElement("h1");
     title.innerHTML = product.title;
-    products.appendChild(title);
+    productItem.appendChild(title);
 
     const price = document.createElement("h2");
     price.innerHTML = `$${product.price}`;
-    products.appendChild(price);
+    productItem.appendChild(price);
 
     const category = document.createElement("span");
     category.innerHTML = `Category: ${product.category}`;
-    products.appendChild(category);
+    productItem.appendChild(category);
 
     const description = document.createElement("p");
     description.innerHTML = product.description;
-    products.appendChild(description);
+    productItem.appendChild(description);
 
     const image = document.createElement("img");
     image.src = product.image;
-    products.appendChild(image);
+    productItem.appendChild(image);
+
+    products.appendChild(productItem);
   });
 }
+
+// Infinite Scroll logic
+async function loadProducts() {
+  const loadedProducts = await getProducts(page, limit);
+  productList = [...productList, ...loadedProducts];
+  displayProducts(loadedProducts);
+  page++;
+}
+
+// Reload event
+document.addEventListener("DOMContentLoaded", async () => {
+  await loadProducts();
+});
+
+// Category Filter
+filterInput.addEventListener("input", () => {
+  const filterValue = filterInput.value.trim().toLowerCase();
+  const filteredProducts = productList.filter((product) =>
+    product.category.toLowerCase().includes(filterValue)
+  );
+  products.innerHTML = "";
+  displayProducts(filteredProducts);
+});
+
+// scroll event
+window.addEventListener("scroll", async function () {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.documentElement.scrollHeight
+  ) {
+    await loadProducts();
+  }
+});
